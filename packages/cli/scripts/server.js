c90a7616704acbcbc7605c87e41407a94940f09e
Christian Blaschke <mail@platdesign.de>
2018-09-20 10:17:39 +0200

    updated deps and added start/stop logging

--- packages/cli/scripts/server.js
+++ packages/cli/scripts/server.js
@@ -7,9 +7,26 @@ require.main.paths.unshift(process.cwd() + '/node_modules');
 const boot = require.main.require('@fragmento/server-hapi');
 
 
-;(async () => {
+;
+(async() => {
 
-	let server = await boot(process.cwd());
-	console.log(server.info);
+	try {
+		console.log('[server-hapi] starting ...');
+		let server = await boot(process.cwd());
+		console.log('[server-hapi] ready\n', server.info);
+
+
+
+		process.once('SIGUSR2', async function() {
+			console.log('[server-hapi] stopping ...');
+			await server.stop();
+			//await new Promise(resolve => setTimeout(resolve, 1000));
+			console.log('[server-hapi] stopped');
+			process.kill(process.pid, 'SIGUSR2');
+		});
+	} catch (e) {
+		console.log('retry', e)
+		process.exit(1);
+	}
 
 })();
